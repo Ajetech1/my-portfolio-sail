8e6a5c573a0438f56e4ddf56a3ff1d83811fa893
Ajetech1 <jeleelajetunmobi@gmail.com>
2024-09-09 20:01:42 +0100

    working on contact form

--- scripts.js
+++ scripts.js
@@ -11,14 +11,37 @@ function closeMenu() {
 
 // contact form submitting to google sheet begin here
 
+// const scriptURL =
+//   "https://script.google.com/macros/s/AKfycbyhyGAxGn0ptcdxpi1ubUWqsA6Bda5MRIhVDJfE5Wci1trCo14MZj8FoImAajjVhDj1Sg/exec";
+// const form = document.forms["submit-to-google-sheet"];
+
+// const msg = document.getElementById("msg");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) => {
+//       msg.innerHTML = "Message sent successfully";
+//       setTimeout(function () {
+//         msg.innerHTML = "";
+//       }, 5000);
+//       form.reset();
+//     })
+//     .catch((error) => console.error("Error!", error.message));
+// });
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbyhyGAxGn0ptcdxpi1ubUWqsA6Bda5MRIhVDJfE5Wci1trCo14MZj8FoImAajjVhDj1Sg/exec";
 const form = document.forms["submit-to-google-sheet"];
-
 const msg = document.getElementById("msg");
+const spinner = document.getElementById("spinner");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // Show the spinner
+  spinner.style.display = "inline-block";
+
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
       msg.innerHTML = "Message sent successfully";
@@ -26,6 +49,12 @@ form.addEventListener("submit", (e) => {
         msg.innerHTML = "";
       }, 5000);
       form.reset();
+      // Hide the spinner after response
+      spinner.style.display = "none";
     })
-    .catch((error) => console.error("Error!", error.message));
+    .catch((error) => {
+      console.error("Error!", error.message);
+      // Hide the spinner in case of error
+      spinner.style.display = "none";
+    });
 });
